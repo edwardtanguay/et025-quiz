9a703185e3026a3dc5d7e473b1e0b324f2d42d1c
Edward Tanguay on dell001 <edward@tanguay.info>
2023-09-13 12:19:24 +0200

    updated files so that site runs

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,119 +1,81 @@
-import { useState } from "react"; // وارد کردن useState
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectAllQuestions } from "../reducers/quiz/QuizReducer";
-import { RootState } from "../store";
 import AlertDelete from "./AlertDelete";
 import UsersInfo from "./UsersInfo";
+import { useState } from "react";
 
 const Questions = () => {
-	const dispatch = useDispatch();
+  const questions = useSelector(selectAllQuestions);
+  const [userAnswers, setUserAnswers] = useState<{ [questionId: number]: string[] }>({});
 
-	const getType = (answer: string) => {
-		return `${answer === "true" || answer === "false" ? "radio" : "checkbox"}`;
-	};
+  const handleAnswerChange = (questionId: number, selectedAnswer: string) => {
+    const updatedUserAnswers = { ...userAnswers };
 
- 
-	const [userAnswers, setUserAnswers] = useState<{ [questionId: number]: string[] }>({});
+    if (updatedUserAnswers[questionId]) {
+      if (updatedUserAnswers[questionId].includes(selectedAnswer)) {
+        updatedUserAnswers[questionId] = updatedUserAnswers[questionId].filter(
+          (answer) => answer !== selectedAnswer
+        );
+      } else {
+        updatedUserAnswers[questionId].push(selectedAnswer);
+      }
+    } else {
+      updatedUserAnswers[questionId] = [selectedAnswer];
+    }
 
-	const questions = useSelector(selectAllQuestions);
+    setUserAnswers(updatedUserAnswers);
+  };
+console.log(userAnswers)
 
- 
-	const handleAnswerChange = (questionId: number, selectedAnswer: string) => {
-	
-		const updatedUserAnswers = { ...userAnswers };
-
- 
-		if (updatedUserAnswers[questionId]) {
-			if (updatedUserAnswers[questionId].includes(selectedAnswer)) {
-			 
-				updatedUserAnswers[questionId] = updatedUserAnswers[questionId].filter((answer) => answer !== selectedAnswer);
-			} else {
-			 
-				updatedUserAnswers[questionId].push(selectedAnswer);
-			}
-		} else {
-	 
-			updatedUserAnswers[questionId] = [selectedAnswer];
-		}
-
-	
-		setUserAnswers(updatedUserAnswers);
-	};
-
-
-const submit=()=>{
-//  const a=Object.values(userAnswers)
-//  a.map((s)=>console.log(s))
-console.log(userAnswers);
-console.log(questions);
-
-	for (const question of questions) {
-		switch (question.type) {
-			case 'trueFalse':
-				// you know what kind of question this is
-				break;
-			case 'singleAnswer':
-				// you know what kind of question this is
-				break;
-			case 'mulitpleAnswer':
-				// you know what kind of question this is
-				break;
-
-		}
-	}
- 
-}
-	return (
-		<div className="flex flex-col gap-8">
-			<div className="flex bg-BLUE500/90 py-4 px-12 justify-between shadow-lg shadow-BACKGROUND_DARK font-Viga md:text-2xl fixed w-full right-0 rounded-b-full">
-				<UsersInfo />
-			</div>
-			<div className="wrapper my-24 flex flex-col gap-6">
-				{questions.map((question) => (
-					<div
-						className="  bg-FOREGROUND px-8 py-6 rounded-lg shadow-lg shadow-BACKGROUND_DARK "
-						key={question.id}
-					>
-						<div className="flex justify-between mb-3 lg:items-center lg:pt-0">
-							<h3 className="text-lg pt-4 font-Viga lg:text-2xl">
-								{question.question}
-							</h3>
-							<h5 className="text-xs text-GRAY400">{question.score}points</h5>
-						</div>
-						<div className="question ">
-							{question.answers.map((answer, index) => (
-								<div
-									className="flex items-center gap-2 mb-2 text-xl"
-									key={index}
-								>
-									<input
-										className="w-5 h-5"
-										type={getType(answer)}
-										name={`question-${question.id}`}
-										id={`question-${answer}`}
-										value={answer}
-										// هنگامی که تغییرات در ورودی رخ می‌دهد، تابع handleAnswerChange را صدا بزنید
-										onChange={() => handleAnswerChange(question.id, answer)}
-										// برای checkbox ها، چک کنید که آیا این پاسخ در پاسخ‌های کاربر برای این سوال وجود دارد یا نه و مطابق با آن تیک بگذارید
-										checked={userAnswers[question.id]?.includes(answer) || false}
-									/>
-									<p>{answer}</p>
-								</div>
-							))}
-						</div>
-					</div>
-				))}
-				<div className="flex justify-between items-center">
-					<button className="bg-GREEN600 text-FOREGROUND hover:text-GREEN600 hover:bg-FOREGROUND  px-8 py-2 rounded-lg font-Viga duration-300 shadow-lg shadow-BACKGROUND_DARK" onClick={submit}>
-						Submit
-					</button>
-					<div>
-						<AlertDelete />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex bg-BLUE700/90 py-4 px-12 justify-between shadow-lg shadow-BACKGROUND_DARK font-Viga md:text-2xl fixed w-full right-0 rounded-b-full">
+        <UsersInfo />
+      </div>
+      <div className="wrapper my-24 flex flex-col gap-6 select-none">
+        {questions.map((question, index) => (
+          <div
+            className="  bg-FOREGROUND px-8 py-6 rounded-lg shadow-lg shadow-BACKGROUND_DARK "
+            key={index}
+          >
+            <div className="flex justify-between mb-3 lg:items-center lg:pt-0">
+              <h3 className="text-lg pt-4 font-Viga lg:text-2xl">
+               {index + 1 + "-"} {question.question}
+              </h3>
+              <h5 className="text-xs text-GRAY600">{question.score}points</h5>
+            </div>
+            <div className="question ">
+              {question.choices.split(",").map((choice, index) => (
+                <div
+                  className="flex items-center gap-2 mb-2 text-xl"
+                  key={index}
+                >
+                  <input
+                    className="w-5 h-5"
+                    type={question.type === "trueFalse" ? "radio" : "checkbox"}
+                    name={`question-${question.id}`}
+                    id={`question-${choice}`}
+                    value={choice}
+                    checked={userAnswers[question.id]?.includes(choice) || false}
+                    onChange={() => handleAnswerChange(question.id, choice)}
+                  />
+                  <p>{choice.split("*")}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <div className="flex justify-between items-center">
+          <button className="bg-GREEN600 text-FOREGROUND hover:text-GREEN600 hover:bg-FOREGROUND  px-8 py-2 rounded-lg font-Viga duration-300 shadow-lg shadow-BACKGROUND_DARK">
+            Submit
+          </button>
+          <div>
+            <AlertDelete />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Questions;
